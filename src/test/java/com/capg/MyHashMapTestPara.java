a0e716c1b9278d6922b68b2d056d091dad6295f0
mehakjit <mehakjitsingh@gmail.com>
2020-10-17 05:22:05 +0530

    [Mehakjit] Created: Ability to find frequency in Paragraph

---
+++ src/test/java/com/capg/MyHashMapTestPara.java
@@ -0,0 +1,25 @@
+package com.capg;
+
+import org.junit.Test;
+
+public class MyHashMapTestPara {
+	@Test
+	public void TestParaInHashMap() {
+		String sen = "Paranoids are not paranoid beacuse they are paranoid but "
+				+ "because they keep putting themselves deliberately into " + "paranoid avoidable situations ";
+
+		MyHashMapPara<String, Integer> mylistpara = new MyHashMapPara<>();
+		String[] words = sen.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = mylistpara.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			mylistpara.add(word, value);
+		}
+		System.out.println(mylistpara);
+		int freq = mylistpara.get("paranoid");
+
+	}
+}
